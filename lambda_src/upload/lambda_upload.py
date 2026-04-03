414cca01c49671069a2d860b3ab4f4a2075368b2
Muhammad Sahlan <muhammadsahlan2008@gmail.com>
2026-04-03 09:00:49 +0700

    Create lambda_upload.py

---
+++ lambda_src/upload/lambda_upload.py
@@ -0,0 +1,11 @@
+import json
+
+def lambda_handler(event, context):
+    print("Upload event:", event)
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "Upload Lambda OK"
+        })
+    }
